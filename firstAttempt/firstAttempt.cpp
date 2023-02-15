8aad48d2ac655addc5bb26d0975f9ea2b1e23c3e
jacob <jalbert90@gmail.com>
2023-02-14 23:14:48 -0500

    Pfffssttt

--- firstAttempt/firstAttempt.cpp
+++ firstAttempt/firstAttempt.cpp
@@ -15,11 +15,26 @@ void solve() {
 		cin >> a[i];
 	}
 
-	for (int i = 0; i < n - k + 1; i++) {
+	for (int i = 0; i < n - 2; i++) {
 		if (a[i] < a[i + 1] && a[i + 1] > a[i + 2]) {
 			p.push_back(i);
 		}
 	}
+
+	for (int i = 0; i < n - k + 1; i++) {
+		int count = 0;
+		for (auto el : p) {
+			if (el > i && el < (i + k - 1)) {
+				count++;
+			}
+		}
+		pair<int, int> temp = make_pair(count, i);
+		maxPeaks.push_back(temp);
+	}
+
+	auto maxIt = max_element(maxPeaks.begin(), maxPeaks.end(), [](pair<int, int>& A, pair<int, int>& B) {return A.first < B.first; });
+
+	cout << maxIt->first + 1 << " " << maxIt->second + 1;
 }
 
 int main() {
