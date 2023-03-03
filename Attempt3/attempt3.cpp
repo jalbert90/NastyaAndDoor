847db39f5512ebfb79ac053c3fefd6d3a65da331
jacob <jalbert90@gmail.com>
2023-03-03 16:56:59 -0500

    Attempt3

---
+++ Attempt3/attempt3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct pSum {
+	int index, sum;
+};
+
+void solve() {
+	int n, k;
+	cin >> n >> k;
+
+	vector<int> a(n + 1);
+
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+}
+
+int main() {
+	int t;
+	cin >> t;
+
+	while (t--) {
+		solve();
+	}
+}
