37f1251160c0515c2accee4af329c51875754a30
jacob <jalbert90@gmail.com>
2023-03-03 17:09:24 -0500

    Prefix sums

--- Attempt3/attempt3.cpp
+++ Attempt3/attempt3.cpp
@@ -12,9 +12,16 @@ void solve() {
 	cin >> n >> k;
 
 	vector<int> a(n + 1);
+	vector<int> pS(n + 1);
 
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
+	}
+
+	for (int i = 1; i <= (n - 2); i++) {
+		if (a[i] < a[i + 1] && a[i + 1] > a[i + 2]) {
+			pS[i + 1] = pS[i] + 1;
+		}
 	}
 }
 
